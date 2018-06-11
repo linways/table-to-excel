8723a51a303873934afda25ee2bb72f66535c826
Rohith <rohith@linways.com>
2018-06-11 20:12:23 +0530

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'dist/xlsx_html_utils.min.js': ['src/xlsx_html_utils.js']
+        }
+      }
+    },
+    concat:{
+      dist: {
+      src: ['lib/xlsx.core.min.js', 'lib/FileSaver.min.js', 'dist/xlsx_html_utils.min.js'],
+      dest: 'dist/xlsx_html.full.min.js',
+      }
+    },
+    watch: {
+      files: ['src/**/*.js'],
+      tasks: ['uglify']
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('default', ['uglify','concat']);
+};
